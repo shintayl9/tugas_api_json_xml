2c9d5ac1975e2041d0d25993c1fc3795e928fd00
Shinta Yulistiana <shintayulistiana2004@gmail.com>
2024-10-21 03:18:24 +0700

    up

--- json-api/api_xml.php
+++ json-api/api_xml.php
@@ -1,9 +1,19 @@
-<?php 
+<?php
 header('Content-Type: application/xml');
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+if ($data) {
+    $id = $data['id'];
+    $judul = $data['Judul'];
+    $penulis = ['Penulis'];
+}
 
 $xml = new SimpleXMLElement('<persons/>');
 $person = $xml->addChild('person');
 $person->addChild('id', 1);
 $person->addChild('Judul', 'Bumi manusia');
 $person->addChild('Penulis', "Pramoedya Ananta Toer");
+
 echo $xml->asXML();
